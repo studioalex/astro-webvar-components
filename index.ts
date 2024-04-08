67cfb3c5c7a14f798c5259eeaff44c02124bb41d
Alexander Sedeke <alexander.sedeke@gmail.com>
2024-04-08 12:56:39 +0200

    init project with basic components

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { default as Badge } from "./src/Badge.astro";
+export { default as Button } from "./src/Button.astro";
+export { default as Clipboard } from "./src/Clipboard.astro";
+export { default as Link } from "./src/Link.astro";
+export { default as NavLink } from "./src/NavigationLink.astro";
